1758b0394e713707b144a7edf4967c1663e0ccdd
Alizon Kingslen <kingslenalizon@icloud.com>
2025-04-15 16:22:24 -0400

    deleted dotenv from server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import express from "express"
 import cors from "cors"
 import { router } from "./routes/router"
-import dotenv from "dotenv"
 
-dotenv.config()
+
 const app = express()
 app.use(cors())
 app.use(express.json())
